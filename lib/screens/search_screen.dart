96a0baf7525be6dfdbc0202663b675d0994b579c
sho621bake <sho6217@gmail.com>
2023-09-17 22:45:34 +0900

    [add]記事詳細画面作成

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -21,53 +21,43 @@ class _SearchScreenState extends State<SearchScreen> {
       appBar: AppBar(
         title: const Text('Qiita Search'),
       ),
-      body: Column(
-        children: [
-          //検索ボックス
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 12,
-                horizontal: 36
-              ),
-              child: TextField(
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Colors.black
-                ),
-                decoration: const InputDecoration(
-                  hintText: '検索ワードを入力してください'
-                ),
-                onSubmitted: (String value) async {
-                  final results = await searchQiita(value);
-                  setState(() =>articles = results);
-                },
-              ),
+      body: Column(children: [
+        //検索ボックス
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 36),
+          child: TextField(
+            style: const TextStyle(fontSize: 18, color: Colors.black),
+            decoration: const InputDecoration(hintText: '検索ワードを入力してください'),
+            onSubmitted: (String value) async {
+              final results = await searchQiita(value);
+              setState(() => articles = results);
+            },
+          ),
+        ),
+        ArticleContainer(
+          article: Article(
+            title: 'テスト',
+            user: User(
+              id: 'qii-taro',
+              profileImageUrl:
+                  'https://firebasestorage.googleapis.com/v0/b/gs-expansion-test.appspot.com/o/unknown_person.png?alt=media',
             ),
-            ArticleContainer(
-              article: Article(
-                title: 'テスト',
-                user: User(
-                  id: 'qii-taro',
-                  profileImageUrl: 'https://firebasestorage.googleapis.com/v0/b/gs-expansion-test.appspot.com/o/unknown_person.png?alt=media',
-                ),
-                createdAt: DateTime.now(),
-                tags: ['Flutter', 'dart'],
-                url: 'https://example.com',
-              ),        
-            )
-          //検索結果一覧
-        ]
-      ),
+            createdAt: DateTime.now(),
+            tags: ['Flutter', 'dart'],
+            url: 'https://example.com',
+          ),
+        )
+        //検索結果一覧
+      ]),
     );
   }
 }
+
 Future<List<Article>> searchQiita(String keyword) async {
   // 1. http通信に必要なデータを準備をする
   // - URL,クエリパラメーターの設定
-  final uri = Uri.https('qiita.com','/api/v2/items', {
-    'query': 'title:$keyword',
-    'per_page': '10'
-  });
+  final uri = Uri.https('qiita.com', '/api/v2/items',
+      {'query': 'title:$keyword', 'per_page': '10'});
   // -アクセストークンの取得
   final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
 
--- lib/widgets/article_container.dart
+++ lib/widgets/article_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:qiita_search_app/models/article.dart';
+import 'package:qiita_search_app/screens/article_screen.dart';
 
 class ArticleContainer extends StatelessWidget {
   const ArticleContainer({
@@ -13,96 +14,102 @@ class ArticleContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 12,
-        horizontal: 16,
-      ),
-      child: Container(
         padding: const EdgeInsets.symmetric(
-          // 内側の余白を指定
-          horizontal: 20,
-          vertical: 16,
+          vertical: 12,
+          horizontal: 16,
         ),
-        decoration: const BoxDecoration(
-          color: Color(0xFF55C500), // 背景色を指定
-          borderRadius: BorderRadius.all(
-            Radius.circular(32), // 角丸を設定
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // 投稿日
-            Text(
-              DateFormat('yyyy/MM/dd').format(article.createdAt),
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 12,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: ((context) => ArticleScreen(article: article))));
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              // 内側の余白を指定
+              horizontal: 20,
+              vertical: 16,
+            ),
+            decoration: const BoxDecoration(
+              color: Color(0xFF55C500), // 背景色を指定
+              borderRadius: BorderRadius.all(
+                Radius.circular(32), // 角丸を設定
               ),
             ),
-            // タイトル
-            Text(
-              article.title,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            // タグ
-            Text(
-              '#${article.tags.join(' #')}',
-              style: const TextStyle(
-                fontSize: 12,
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // ハートアイコンといいね数
-                Column(
-                  children: [
-                    const Icon(
-                      Icons.favorite,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      article.likeCount.toString(),
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
+                // 投稿日
+                Text(
+                  DateFormat('yyyy/MM/dd').format(article.createdAt),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
                 ),
-                // 投稿者のアイコンと投稿者名
-                Column(
+                // タイトル
+                Text(
+                  article.title,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                // タグ
+                Text(
+                  '#${article.tags.join(' #')}',
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    CircleAvatar(
-                      radius: 26,
-                      backgroundImage: NetworkImage(article.user.profileImageUrl),
+                    // ハートアイコンといいね数
+                    Column(
+                      children: [
+                        const Icon(
+                          Icons.favorite,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          article.likeCount.toString(),
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 4),
-                    Text(
-                      article.user.id,
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.white,
-                      ),
+                    // 投稿者のアイコンと投稿者名
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        CircleAvatar(
+                          radius: 26,
+                          backgroundImage:
+                              NetworkImage(article.user.profileImageUrl),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          article.user.id,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
